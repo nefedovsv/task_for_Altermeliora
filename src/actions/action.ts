92b763c6b6d9518af8406114a1c8cb27d2028fb3
nefedovsv <47773817+nefedovsv@users.noreply.github.com>
2020-02-27 12:23:26 +0300

    change button style

--- src/actions/action.ts
+++ src/actions/action.ts
@@ -1,60 +1,64 @@
-import {LOG_IN, SIGN_UP} from "../utils/constants";
-import {ISignUpAction, signFromSaga} from "./signUp";
-import {ILogInAction, logFromSaga} from "./logIn";
-import { SagaIterator } from '@redux-saga/core';
-import { fork, call, takeEvery, select, put } from 'redux-saga/effects';
-import {IState, IUser} from "../interfaces/interface";
-import {delay, getStore, makeErrorMsg, makeSuccessMsg, selectUser} from "../utils/helpers";
+import { LOG_IN, SIGN_UP } from "../utils/constants";
+import { ISignUpAction, signFromSaga } from "./signUp";
+import { ILogInAction, logFromSaga } from "./logIn";
+import { SagaIterator } from "@redux-saga/core";
+import { fork, call, takeEvery, select, put } from "redux-saga/effects";
+import { IState, IUser } from "../interfaces/interface";
+import {
+  delay,
+  getStore,
+  makeErrorMsg,
+  makeSuccessMsg,
+  selectUser
+} from "../utils/helpers";
 import { loaded } from "./loaded";
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function* rootSaga(): SagaIterator {
-	yield fork(Saga);
+  yield fork(Saga);
 }
 
 export default function* Saga() {
-	yield takeEvery(LOG_IN, logInSaga);
-	yield takeEvery(SIGN_UP, signUpSaga);
+  yield takeEvery(LOG_IN, logInSaga);
+  yield takeEvery(SIGN_UP, signUpSaga);
 }
 
 export function* signUpSaga(action: ISignUpAction) {
+  const state: IState = yield select(getStore);
 
-	const state:IState = yield select(getStore)
+  const sameUser: IUser[] = selectUser(state, action.payload);
 
-	const sameUser:IUser[] = selectUser(state, action.payload)
+  yield delay(2000);
 
-	yield delay(2000)
-
-	try {
-		if(sameUser.length>=1){
-			yield call(makeSuccessMsg)
-			yield put(loaded())
-		} else{
-			yield put(signFromSaga(action.payload))
-			yield call(makeSuccessMsg)
-		}
-	} catch (e) {
-		yield call(makeErrorMsg)
-	}
+  try {
+    if (sameUser.length >= 1) {
+      yield call(makeSuccessMsg);
+      yield put(loaded());
+    } else {
+      yield put(signFromSaga(action.payload));
+      yield call(makeSuccessMsg);
+    }
+  } catch (e) {
+    yield call(makeErrorMsg);
+  }
 }
 
 export function* logInSaga(action: ILogInAction) {
+  const state: IState = yield select(getStore);
 
-	const state:IState = yield select(getStore)
+  const user: IUser[] = selectUser(state, action.payload);
 
-	const user:IUser[] = selectUser(state, action.payload)
+  yield delay(2000);
 
-	yield delay(2000)
-
-	try {
-		if(user.length){
-			yield put(logFromSaga(user[0]))
-			yield call(makeSuccessMsg)
-		} else {
-			yield put(loaded())
-			yield call(makeErrorMsg)
-		}
-	} catch (e) {
-		yield call(makeErrorMsg)
-	}
+  try {
+    if (user.length) {
+      yield put(logFromSaga(user[0]));
+      yield call(makeSuccessMsg);
+    } else {
+      yield put(loaded());
+      yield call(makeErrorMsg);
+    }
+  } catch (e) {
+    yield call(makeErrorMsg);
+  }
 }
-
--- src/components/LanguageContainer/LanguageElement.tsx
+++ src/components/LanguageContainer/LanguageElement.tsx
@@ -1,27 +1,25 @@
-import React from 'react';
-import { Radio } from 'antd';
-import { RadioChangeEvent } from 'antd/lib/radio';
-import { useTranslation } from 'react-i18next';
-import {Language} from "../../interfaces/interface";
+import React from "react";
+import { Radio } from "antd";
+import { RadioChangeEvent } from "antd/lib/radio";
+import { useTranslation } from "react-i18next";
+import { Language } from "../../interfaces/interface";
 import styles from "./Language.module.css";
 
 export const LanguageElement: React.FC<{}> = () => {
+  const { Group, Button } = Radio;
 
-	const { Group, Button } = Radio;
+  const { i18n } = useTranslation();
 
-	const { i18n } = useTranslation();
+  const onChange = (e: RadioChangeEvent): void => {
+    i18n.changeLanguage(e.target.value);
+  };
 
-	const onChange = (e: RadioChangeEvent): void => {
-		i18n.changeLanguage(e.target.value)
-	}
-
-	return (
-		<div className={styles.body}>
-			<Group onChange={onChange} defaultValue={Language.ENG} size="small" >
-				<Button value={Language.ENG} className={styles.button}>{Language.ENG}</Button>
-				<Button value={Language.RUS} className={styles.button}>{Language.RUS}</Button>
-			</Group>
-		</div>
-)
-}
-
+  return (
+    <div className={styles.body}>
+      <Group onChange={onChange} defaultValue={Language.ENG} size="small">
+        <Button value={Language.ENG}>{Language.ENG}</Button>
+        <Button value={Language.RUS}>{Language.RUS}</Button>
+      </Group>
+    </div>
+  );
+};
